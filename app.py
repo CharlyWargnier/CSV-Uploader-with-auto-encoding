d782fd00c6e5b29fc2cf209c3fbd6611e5675ce4
Charly Wargnier <cwar05@gmail.com>
2021-11-04 20:16:22 +0000

    Added files

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import chardet
+
+st.title("💡 CSV Uploader with auto-encoding")
+
+uploaded_file = st.file_uploader("Choose a CSV file")
+
+if uploaded_file is not None:
+
+    result = chardet.detect(uploaded_file.getvalue())
+    # Get encoding value
+    encoding_value = result["encoding"]
+    df = pd.read_csv(
+        uploaded_file,
+        encoding=encoding_value,
+    )
+    # Print encoding value
+    st.caption("File encoding is 👉'" + str(encoding_value) + "'")
+    st.write(df)
